ffbf78b41544fb259a0ff9dd660c8fef8bb2abad
wanghua <1567832p@gmail.com>
2026-09-19 18:13:05 +0800

    chore: 移除误入源码树的临时脚本

--- control-app/src/main/java/p6-headref.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-git grep -l -E "EvalComparisonAutoRecorder|ReportWritingRubric|RootCauseZhDictionary|ModelFailureGuide|ApprovalQueueFollowUp|MutationToolCatalog|ToolDescriptionZh|RootCauseCatalogPort|ArenaChaosDrillRecovery|CompositeDrillRecovery|DrillRecoveryPort|FlagdDrillRecovery|SixElementsChecker" HEAD -- "control-app/src/main/java" 2>&1
